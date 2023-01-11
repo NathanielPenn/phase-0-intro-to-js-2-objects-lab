9e3478bf2aa7939dcff8fa7ed89e82827fc4913f
NatePennebaker <npenne1@aol.com>
2023-01-11 10:58:35 -0500

    commit tree 20fc261be24647f7badb3dd5998231c7ec386b6b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Rick',
+    streetAddress: '221B Baker Street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee['streetAddress'] = "11 Broadway";
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee['streetAddress'] = "12 Broadway";
+   return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee['name'];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee['name'];
+    return employee;
+}
